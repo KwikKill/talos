e68a859bd5ad2e219b5ec7b71a5038aada064eb1
KwikKill <gabriel@blaisot.org>
2025-05-13 00:29:55 +0100

    Improve post it placement

--- components/post-it.tsx
+++ components/post-it.tsx
@@ -6,14 +6,15 @@ interface PostItProps {
 export default function PostIt({ username, password }: PostItProps) {
   return (
     <div>
-      <div className="fixed right-16 -bottom-20 z-[200] h-32 w-50 rotate-[350deg] transform bg-yellow-300 p-3 shadow-md">
+      <div className="absolute right-0 -bottom-24 z-[200] h-32 w-50 rotate-[350deg] transform bg-yellow-300 p-3 shadow-md">
         <div className="font-handwriting text-sm text-gray-800 pointer-events-none select-none">
           <p>{username}/{password}</p>
         </div>
       </div>
-      <div className="fixed right-56 -bottom-20 z-[200] h-32 w-50 rotate-[6deg] transform bg-yellow-300 p-3 shadow-md">
+      <div className="absolute right-40 -bottom-24 z-[200] h-32 w-50 rotate-[6deg] transform bg-yellow-300 p-3 shadow-md">
         <div className="font-handwriting text-sm text-gray-800 pointer-events-none select-none">
-          <b>Don't forget your credentials!</b>
+          <b>Please, remove the post-it note</b>
+          <p>signed : Your boss</p>
         </div>
       </div>
     </div>
